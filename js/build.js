b0f2b1b7c62dd318a4c715b382f78006cfa00b90
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-10-19 15:27:58 +0100

    add

--- js/build.js
+++ js/build.js
@@ -6,24 +6,39 @@ export const addPost = (post) => {
 
     const title = document.createElement("a")
     title.href = post.url
+    title.target = "_blank"
+    title.classList.add("title")
     title.textContent = post.title
-    title.style.display = "block"
     postEl.append(title)
+
+    const author = document.createElement("p")
+    author.classList.add("author")
+    author.textContent = post.by
+    postEl.prepend(author)
 
     const bottomSection = document.createElement("div")
     bottomSection.classList.add("bottom-section")
 
-    const author = document.createElement("span")
-    author.classList.add("author")
-    author.textContent = post.by
-    bottomSection.appendChild(author)
-
     const comments = document.createElement("span")
     comments.classList.add("comments")
     comments.textContent = "Comments: "+ (post.kids ? post.kids.length : 0)
+    if (!post.kids) {
+        comments.title = "No Comments"
+    }    
     bottomSection.append(comments)
 
+    const date = document.createElement("span")
+    date.classList.add("date")
+    date.textContent = new Date(post.time*1000).toDateString()
+    bottomSection.append(date)
+
     postEl.append(bottomSection)
+
+    const type = document.createElement("span")
+    type.textContent = post.type
+    type.classList.add("type")
+    type.style.background = ({story:"#5bad00", poll:"blue", comment: "green", job:"crimson"})[post.type] || "black"
+    postEl.append(type)
 
     parent.append(postEl)
 }
